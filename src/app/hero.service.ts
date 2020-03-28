445d75d339e13d00318fc7eafe120d0f3809bdb6
Patrick Lombardo <patrick_lombardo@comcast.com>
2020-03-27 23:26:31 -0400

    build out messages service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
+
+import { MessageService } from "./message.service";
+
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
-import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   // Returns heroes from the mock file
   // This is a good place to make ajax requests
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add("HeroService: fetched heroes");
     return of(HEROES);
   }
 }
