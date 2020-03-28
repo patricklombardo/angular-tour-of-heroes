6473818b5c447d508a66dc6bf579f47782f43ade
Patrick Lombardo <patrick_lombardo@comcast.com>
2020-03-28 00:19:59 -0400

    mock api calls

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,23 +4,31 @@ import { Observable, of } from "rxjs";
 import { MessageService } from "./message.service";
 
 import { Hero } from "./hero";
-import { HEROES } from "./mock-heroes";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  private heroesUrl = "api/heroes";
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
 
   // Returns heroes from the mock file
   // This is a good place to make ajax requests
   getHeroes(): Observable<Hero[]> {
     this.messageService.add("HeroService: fetched heroes");
-    return of(HEROES);
+    this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
