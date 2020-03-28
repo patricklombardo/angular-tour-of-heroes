64ca01fc74fba50d7ccf06930be2b6c4262c72d4
Patrick Lombardo <patrick_lombardo@comcast.com>
2020-03-28 00:08:03 -0400

    move detail component to separate route

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,20 +14,11 @@ export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
 
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(data => (this.heroes = data));
   }
