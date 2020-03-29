8ebb68ab83a301be15e5e05f36c311c0e948fe2c
Patrick Lombardo <patrick_lombardo@comcast.com>
2020-03-29 01:03:33 -0400

    Revert "mock api calls"
    This reverts commit 6473818b5c447d508a66dc6bf579f47782f43ade.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,31 +4,23 @@ import { Observable, of } from "rxjs";
 import { MessageService } from "./message.service";
 
 import { Hero } from "./hero";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HEROES } from "./mock-heroes";
 
 @Injectable({
   providedIn: "root"
 })
 export class HeroService {
-  private heroesUrl = "api/heroes";
-  constructor(
-    private messageService: MessageService,
-    private http: HttpClient
-  ) {}
+  constructor(private messageService: MessageService) {}
 
   // Returns heroes from the mock file
   // This is a good place to make ajax requests
   getHeroes(): Observable<Hero[]> {
     this.messageService.add("HeroService: fetched heroes");
-    this.http.get<Hero[]>(this.heroesUrl);
+    return of(HEROES);
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
-  }
-
-  private log(message: string) {
-    this.messageService.add(`HeroService: ${message}`);
   }
 }
